25a9aad90c7435bbd413df339c40597b4132f1b2
lenovo <lenovo@DESKTOP-G3RV5V6>
2024-07-20 15:43:28 +0530

    OOPS - Inharitance and Encapsulaton

---
+++ ConsoleApp_Batch_11_30/oops/EncapsulationExample.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace ConsoleApp_Batch_11_30.oops
+{
+    class Employee
+    {
+        private int _Code;
+        private string _Name;
+
+        public Employee()
+        {
+            Random random = new Random();
+            _Code = random.Next(100, 999);
+        }
+
+        public int Code
+        {
+            get { return _Code; }
+        }
+
+        public string Name
+        {
+            set
+            {
+                if (!(string.IsNullOrEmpty(value) || string.IsNullOrWhiteSpace(value)))
+                    _Name = value;
+                else
+                    throw new Exception("Name property is invalid!");
+            }
+            get { return _Name; }
+        }
+
+
+        //------------------Old One-------------------------
+        //public int GetCode()
+        //{
+        //    return _Code;
+        //}
+
+        //public void SetName(string name)
+        //{
+        //    if (!(string.IsNullOrEmpty(name) || string.IsNullOrWhiteSpace(name)))
+        //        _Name = name;
+        //    else
+        //        throw new Exception("Name property is invalid!");
+
+        //}
+
+        //public string GetName()
+        //{
+        //    return _Name;
+        //}
+    }
+    internal class EncapsulationExample
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee()
+            {
+                Name = "Test"
+            };
+            //emp.Code = -1010;
+            //emp.Name = "   ";
+
+            //emp.SetName("Test");
+            //Console.WriteLine($"Code : {emp.GetCode()} Name : {emp.GetName()}");
+
+            //emp.Name = "Test";
+            Console.WriteLine($"Code : {emp.Code} Name : {emp.Name}");
+
+            //Save emp
+        }
+    }
+}
---
+++ ConsoleApp_Batch_11_30/oops/InheritanceExamle.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_Batch_11_30.oops
+{
+    //class GoldLoan
+    //{
+    //    private string _AccountNumber;
+    //    private string _AccounntHolder;
+    //    private float _Weight;
+    //    private float _LoanAmount;
+    //    private float _ROI;
+    //    private int _NumbersOfOrnements;
+
+    //}
+
+    //class PersonalLoan
+    //{
+    //    private string _AccountNumber;
+    //    private string _AccounntHolder;
+    //    //private float _Weight;
+    //    private float _LoanAmount;
+    //    private float _ROI;
+    //   // private int _NumbersOfOrnements;
+    //}
+
+    class Loan
+    {
+        private string _AccountNumber;
+        private string _AccounntHolder;
+        private float _LoanAmount;
+        private float _ROI;
+        private int _Tenure;
+    }
+
+    class GoldLoan : Loan
+    {
+        private float _Weight;
+        private int _NumbersOfOrnements;
+    }
+
+    class HomeLoan : Loan
+    {
+        private PropetyInfo Propertidetaisl;
+    }
+
+    class PropetyInfo
+    {
+
+    }
+    internal class InheritanceExamle
+    {
+       
+    }
+}
